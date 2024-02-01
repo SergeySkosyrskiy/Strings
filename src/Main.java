b73c001d975d2b2ba141f6d92966826d19e7768b
SergeySkosyrskiy <s-s-e_96@mail.ru>
2024-02-01 21:02:11 +0800

    Homework

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,29 @@
+import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        String phone = "9243967871";
-        if (phone.()==10 ) {
-            phone = '7' + phone;
-        }
-        System.out.println("phone = " + phone);
+        System.out.println("Задача1");
+
+        String firstName = "Ivan";
+        String midleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName +" " + firstName +" " + midleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+
+        System.out.println("Задача2");
+        System.out.println(fullName.toUpperCase());
+
+        System.out.println("Задача3");
+        firstName = "Семён";
+        midleName = "Семёнович";
+        lastName = "Иванов";
+        /*firstName = firstName.replace("ё", "е");
+        midleName = midleName.replace("ё", "е");
+        lastName = lastName.replace("ё", "е");*/
+        fullName = lastName +" " + firstName +" " + midleName;
+        fullName = fullName.replace("ё", "е");
+        System.out.println(fullName.toUpperCase());
+
+
     }
 }
