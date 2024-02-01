bdb5d13e567ab172855cc508a0abdb0b53b3151f
SergeySkosyrskiy <s-s-e_96@mail.ru>
2024-02-01 19:53:53 +0800

    .

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String phone = "9243967871";
+        if (phone.()==10 ) {
+            phone = '7' + phone;
+        }
+        System.out.println("phone = " + phone);
+    }
+}
